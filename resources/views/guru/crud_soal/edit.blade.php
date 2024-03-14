9d5e721a6352aa760cef09eb34c929e5a3ff5992
heyikhwan <ikhwanmail231000@gmail.com>
2024-03-15 04:48:07 +0700

    update nampilin soal ujian di edit

--- resources/views/guru/crud_soal/edit.blade.php
+++ resources/views/guru/crud_soal/edit.blade.php
@@ -14,11 +14,12 @@
         @endif
 
         <div class="row">
-            <form action="{{ route('guru.ujian.update', $userSoal[0]->id) }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('guru.ujian.update', $data[0]->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT') {{-- Use the PUT method for updates --}}
                 <div class="col-md-12">
-                    <div class="card" style="border: none; box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;">
+                    <div class="card"
+                        style="border: none; box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;">
                         <div class="card-header fs-5 d-none d-sm-inline">
                             Edit Ujian Pilihan Ganda
                         </div>
@@ -27,25 +28,31 @@
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <select name="pelajaran_id" id="pelajaran_id" class="form-control bg-white">
-                                            <option value="{{ $userSoal[0]->pelajaran_id }}" selected>{{ $userSoal[0]->pelajaran }}</option>
+                                            <option value="{{ $data[0]->pelajarans->id }}" selected>{{
+                                                $data[0]->pelajarans->pelajaran
+                                                }}</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <select name="kelas_id" id="kelas_id" class="form-control bg-white">
-                                            <option value="{{ $userSoal[0]->kelas_id }}" selected>{{ $userSoal[0]->nama_kelas }}</option>
+                                            <option value="{{ $data[0]->kelas->kelas_id }}" selected>{{
+                                                $data[0]->kelas->kelas->nama_kelas }}
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
-                                        <input type="number" name="durasi" id="durasi" class="form-control bg-white" placeholder="jam" value="{{$userSoal[0]->durasi }}">
+                                        <input type="number" name="durasi" id="durasi" class="form-control bg-white"
+                                            placeholder="jam" value="{{$data[0]->pelajarans->durasi }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
-                                        <input type="number" class="form-control bg-white" name="durasi_menit" placeholder="menit" value="">
+                                        <input type="number" class="form-control bg-white" name="durasi_menit"
+                                            placeholder="menit" value="">
                                     </div>
                                 </div>
                             </div>
@@ -54,57 +61,83 @@
                 </div>
 
                 <div class="col-md-12 mt-5">
-                    <div class="card" style="border: none; box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;">
+                    <div class="card"
+                        style="border: none; box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;">
                         <div class="card-header bg-dark text-light">
                             <h3>Edit Soal Ujian</h3>
                         </div>
                         <div class="card-body" id="add_soal">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="mb-3">
-                                        <div class="form-floating">
-                                            <textarea class="form-control" placeholder="Leave a comment here" name="data[soal][0][isi_soal][]" id="editor"></textarea>
-                                        </div>      
-                                    </div>                                
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <input type="text" name="data[soal][0][isi_jawaban][]" class="form-control bg-white" placeholder="Pilihan A" >
+                            <div class="d-flex flex-column gap-5">
+                                @foreach ($data as $item)
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="mb-3">
+                                            <div class="form-floating">
+                                                <textarea class="form-control editor" placeholder="Leave a comment here"
+                                                    name="soal[{{ $loop->index }}][isi_soal]">{{ $item->isi_soal }}</textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <input type="text" name="soal[{{ $loop->index }}][isi_jawaban][]"
+                                                class="form-control bg-white" placeholder="Pilihan A"
+                                                value="{{ $item->jawabans[0]->isi_jawaban }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <input type="text" name="soal[{{ $loop->index }}][isi_jawaban][]"
+                                                class="form-control bg-white" placeholder="Pilihan B"
+                                                value="{{ $item->jawabans[1]->isi_jawaban }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <input type="text" name="soal[{{ $loop->index }}][isi_jawaban][]"
+                                                class="form-control bg-white" placeholder="Pilihan C"
+                                                value="{{ $item->jawabans[2]->isi_jawaban }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <input type="text" name="soal[{{ $loop->index }}][isi_jawaban][]"
+                                                class="form-control bg-white" placeholder="Pilihan D"
+                                                value="{{ $item->jawabans[3]->isi_jawaban }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <input type="text" name="soal[{{ $loop->index }}][isi_jawaban][]"
+                                                class="form-control bg-white" placeholder="Pilihan E"
+                                                value="{{ $item->jawabans[4]->isi_jawaban }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        @php
+                                        $jawabans = $item->jawabans->toArray();
+                                        $jawaban_benar = array_search(1, array_column($jawabans, 'is_correct'));
+                                        @endphp
+                                        <select class="form-select bg-white"
+                                            name="soal[{{ $loop->index }}][isi_jawaban_correct]"
+                                            aria-label="Default select example">
+                                            <option selected>Pilih Jawaban</option>
+                                            <option value="0" @selected($jawaban_benar==0)>A</option>
+                                            <option value="1" @selected($jawaban_benar==1)>B</option>
+                                            <option value="2" @selected($jawaban_benar==2)>C</option>
+                                            <option value="3" @selected($jawaban_benar==3)>D</option>
+                                            <option value="4" @selected($jawaban_benar==4 )>E</option>
+                                        </select>
                                     </div>
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <input type="text" name="data[soal][0][isi_jawaban][]" class="form-control bg-white" placeholder="Pilihan B">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <input type="text" name="data[soal][0][isi_jawaban][]" class="form-control bg-white" placeholder="Pilihan C">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <input type="text" name="data[soal][0][isi_jawaban][]" class="form-control bg-white" placeholder="Pilihan D">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <input type="text" name="data[soal][0][isi_jawaban][]" class="form-control bg-white" placeholder="Pilihan E">
-                                    </div>
-                                </div>
+                                @endforeach
                             </div>
-                            <select class="form-select bg-white" name="data[soal][0][isi_jawaban_correct]" aria-label="Default select example">
-                                <option selected>Pilih Jawaban</option>
-                                <option value="0">A</option>
-                                <option value="1">B</option>
-                                <option value="2">C</option>
-                                <option value="3">D</option>
-                                <option value="4">E</option>
-                            </select>
+
+                            <button type="submit" class="btn btn-primary mt-3">Update</button>
+
                         </div>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Update</button>
             </form>
 
         </div>
@@ -114,6 +147,15 @@
 <script src="https://cdn.ckeditor.com/ckeditor5/40.0.0/classic/ckeditor.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
-
+    document.addEventListener("DOMContentLoaded", function() {
+        // set all .editor to ckeditor
+        const editors = document.querySelectorAll('.editor');
+        editors.forEach(editor => {
+            ClassicEditor
+                .create( editor )
+                .catch( error => {
+                    console.error( error );
+                })
+        }) 
+    });
 </script>
-
